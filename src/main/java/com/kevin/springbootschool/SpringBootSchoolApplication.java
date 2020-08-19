808de334a5d8f5a387b82018171aef662cf76270
kevin <363969286@qq.com>
2020-08-19 14:58:56 +0800

    Initial commit

---
+++ src/main/java/com/kevin/springbootschool/SpringBootSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.kevin.springbootschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSchoolApplication.class, args);
+    }
+
+}
